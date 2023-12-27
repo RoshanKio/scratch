22aa5c8dd7d5eeb32e183d470102704949f65eb9
Ram <ram@krishna.com>
2023-12-26 20:47:22 -0500

    mycmt

---
+++ javaStarPattern/src/com/takeo/NepalFlag.java
@@ -0,0 +1,14 @@
+package com.takeo;
+
+public class NepalFlag {
+	public static void main(String... tts) {
+		for(int i = 1; i <= 2; i++) {
+			String str = "";
+			for(int j = 1; j <= 11; j++) {
+				str = str+"*";
+				System.out.println(str);
+			}
+		}
+	}
+
+}
