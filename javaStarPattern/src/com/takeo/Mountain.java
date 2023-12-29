372addfdee4962c9d1237a8d775ba302f4729631
Ram <ram@krishna.com>
2023-12-29 00:22:11 -0500

    msg

---
+++ javaStarPattern/src/com/takeo/Mountain.java
@@ -0,0 +1,19 @@
+package com.takeo;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Mountain {
+	public static void main(String... ts) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("Please enter an Integer number");
+			int n = sc.nextInt();
+			
+			
+		System.out.println("Today is "+LocalDate.now());
+		}
+		
+	}
+
+}
